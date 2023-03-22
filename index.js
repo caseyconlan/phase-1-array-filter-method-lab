11309bd418ebc7a0ea7c5fee33ee08a2ccdf1677
root <root@DESKTOP-1IHRCNC>
2023-03-22 10:29:53 -0700

    7 passed tests

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, lettercase) {
+    return drivers.filter(function (driver) {return driver.toLowerCase() == lettercase.toLowerCase()})
+  }
+
+  function fuzzyMatch(drivers, firstLetter) {
+    return drivers.filter(function (driver) { return driver.toLowerCase().substring(0, firstLetter.length) == firstLetter.toLowerCase() })
+  }
+  
+  function matchName(drivers, dupe) {
+    return drivers.filter(function (driver) { return driver.name == dupe })
+  }
